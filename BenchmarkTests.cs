fe0ce38aea9681d2798770f41f2b701de9fdf359
MarijaBB <101042225+MarijaBB@users.noreply.github.com>
2025-08-23 20:24:34 +0200

    Add project files.

---
+++ BenchmarkTests.cs
@@ -0,0 +1,293 @@
+﻿using System.Diagnostics;
+
+namespace MySimpleDictionary
+{
+    public class BenchmarkTests
+    {
+        private const int SmallSize = 1000;
+        private const int MediumSize = 10000;
+        private const int LargeSize = 100000;
+
+        public static void Main()
+        {
+            RunAllBenchmarks(SmallSize, "Small");
+            RunAllBenchmarks(MediumSize, "Medium");
+            RunAllBenchmarks(LargeSize, "Large");
+        }
+
+        private static void RunAllBenchmarks(int size, string sizeLabel)
+        {
+            Console.WriteLine($"\n{sizeLabel} Dataset - {size} elements:");
+            Console.WriteLine("----------------------");
+
+            BenchmarkAdd(size);
+            BenchmarkLookup(size);
+            BenchmarkIteration(size);
+            BenchmarkRemove(size);
+            BenchmarkGetKeys(size);
+            BenchmarkGetValues(size);
+            BenchmarkClear(size);
+        }
+        private static void BenchmarkAdd(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            var stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < size; i++)
+            {
+                myDict[keys[i]] = values[i];
+            }
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+
+            
+            var stopwatch2 = Stopwatch.StartNew();
+            for (int i = 0; i < size; i++)
+            {
+                dict[keys[i]] = values[i];
+            }
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine("Add operation:");
+            printResults(myDictTime, dictTime);
+        }
+        private static void BenchmarkLookup(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                myDict.Add(keys[i], values[i]);
+                dict.Add(keys[i], values[i]);
+            }
+
+            var lookupKeys = new string[size];
+            for (int i = 0; i < size; i++)
+            {
+                lookupKeys[i] = i % 10 == 0 ? "something" + i : keys[i % keys.Length];
+            }
+
+            int myResults = 0;
+            var stopwatch = Stopwatch.StartNew();
+            foreach (var key in lookupKeys)
+            {
+                if (myDict.ContainsKey(key))
+                    myResults++;
+            }
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+
+            int netResults = 0;
+            var stopwatch2 = Stopwatch.StartNew();
+            foreach (var key in lookupKeys)
+            {
+                if (dict.ContainsKey(key))
+                    netResults++;
+            }
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine("Lookup key operation:");
+            printResults(myDictTime, dictTime);
+        }
+        private static void BenchmarkIteration(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                myDict.Add(keys[i], values[i]);
+                dict.Add(keys[i], values[i]);
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            foreach (var kvp in myDict)
+            {          
+            }
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+        
+            var stopwatch2 = Stopwatch.StartNew();
+            foreach (var pair in dict)
+            {
+            }
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+            Console.WriteLine("Iteration:");
+            printResults(myDictTime, dictTime);
+        }
+
+        private static void BenchmarkRemove(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                myDict.Add(keys[i], values[i]);
+                dict.Add(keys[i], values[i]);
+            }
+
+            var keysToRemove = new List<string>();
+            for (int i = 0; i < size; i += 5)
+            {
+                keysToRemove.Add(keys[i]);
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            foreach (var key in keysToRemove)
+            {
+                myDict.Remove(key);
+            }
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+
+            var stopwatch2 = Stopwatch.StartNew();
+            foreach (var key in keysToRemove)
+            {
+                dict.Remove(key);
+            }
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+
+            Console.WriteLine("Remove operation:");
+            printResults(myDictTime, dictTime);
+
+        }
+
+        private static void BenchmarkGetKeys(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                myDict.Add(keys[i], values[i]);
+                dict.Add(keys[i], values[i]);
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            _ = myDict.Keys;
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+
+            var stopwatch2 = Stopwatch.StartNew();
+            _ = dict.Keys;
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+
+            Console.WriteLine("Get keys operation:");
+            printResults(myDictTime, dictTime);
+        }
+        private static void BenchmarkGetValues(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                myDict.Add(keys[i], values[i]);
+                dict.Add(keys[i], values[i]);
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            _ = myDict.Values;
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+
+            var stopwatch2 = Stopwatch.StartNew();
+            _ = dict.Values;
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+
+            Console.WriteLine("Get values operation:");
+            printResults(myDictTime, dictTime);
+        }
+        private static void BenchmarkClear(int size)
+        {
+            var keys = GenerateTestKeys(size);
+            var values = GenerateTestValues(size);
+
+            var myDict = new MySimpleDictionary<string, int>();
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                myDict.Add(keys[i], values[i]);
+                dict.Add(keys[i], values[i]);
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            myDict.Clear();
+            stopwatch.Stop();
+            var myDictTime = stopwatch.Elapsed.TotalMilliseconds;
+
+            var stopwatch2 = Stopwatch.StartNew();
+            dict.Clear();
+            stopwatch2.Stop();
+            var dictTime = stopwatch2.Elapsed.TotalMilliseconds;
+
+
+            Console.WriteLine("Clear operation:");
+            printResults(myDictTime, dictTime);
+        }
+
+        private static string[] GenerateTestKeys(int count)
+        {
+            var keys = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                keys[i] = $"key{i}";
+            }
+
+            return keys;
+        }
+
+        private static int[] GenerateTestValues(int count)
+        {
+            var values = new int[count];
+            var random = new Random(100);
+
+            for (int i = 0; i < count; i++)
+            {
+                values[i] = random.Next();
+            }
+
+            return values;
+        }
+
+        private static void printResults(double myDictTime, double dictTime)
+        {
+            Console.WriteLine($"MySimpleDictionary: {myDictTime:F2} ms");
+            Console.WriteLine($"Dictionary:         {dictTime:F2} ms");
+            Console.WriteLine($"Ratio:              {myDictTime / dictTime:F2}x\n");
+        }
+    }
+}
---
+++ MySimpleDictionary.cs
@@ -0,0 +1,243 @@
+﻿using System.Collections;
+
+	public class MySimpleDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+	{
+		//fields
+		private List<KeyValuePair<TKey, TValue>>[] buckets;
+		private int numberOfElements;
+
+		public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+		{
+			foreach (var bucket in buckets)
+			{
+				if (bucket != null)
+				{
+					foreach (var pair in bucket)
+					{
+						yield return pair;
+					}
+				}
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		//constructor
+		public MySimpleDictionary(int size = 32)
+		{
+			buckets = new List<KeyValuePair<TKey, TValue>>[size];
+			numberOfElements = 0;
+		}
+		private void Resize()
+		{
+			List<KeyValuePair<TKey, TValue>>[] oldBuckets = buckets;
+			buckets = new List<KeyValuePair<TKey, TValue>>[numberOfElements*2];
+			numberOfElements = 0;
+			foreach (var bucket in oldBuckets)
+			{
+				if (bucket != null)
+				{
+					foreach (var pair in bucket)
+					{
+						var newPosition = HashFunction(pair.Key);
+						if (buckets[newPosition] == null)
+							buckets[newPosition] = new List<KeyValuePair<TKey, TValue>>();
+
+						buckets[newPosition].Add(pair);
+						numberOfElements++;
+					}			
+				}
+			}
+		}
+		private void shouldResize()
+		{
+			if (numberOfElements >= buckets.Length * 0.75)
+			{
+				Resize();
+			}
+		}
+		// [.]
+		public TValue this[TKey key]
+		{
+			get
+			{
+				int position = HashFunction(key);
+				var bucket = buckets[position];
+				if (bucket != null)
+				{
+					foreach (var pair in bucket)
+					{
+						if (pair.Key.Equals(key))
+							return pair.Value;
+					}
+				}
+				throw new KeyNotFoundException($"Key '{key}' not found.");
+			}
+
+			set
+			{
+				if (value == null) 
+					throw new ArgumentNullException(nameof(value));
+				shouldResize();
+				int position = HashFunction(key);
+				if (buckets[position] == null)
+					buckets[position] = new List<KeyValuePair<TKey, TValue>>();
+
+				var bucket = buckets[position];
+				for (int i = 0; i < bucket.Count; i++)
+				{
+					if (bucket[i].Key.Equals(key))
+					{
+						bucket[i] = new KeyValuePair<TKey, TValue>(key, value);
+						return;
+					}
+				}
+				bucket.Add(new KeyValuePair<TKey, TValue>(key, value));
+				numberOfElements++;
+			}
+		}
+
+		//get number of elements in dictionary
+		public int Count()
+		{
+			return numberOfElements;
+		}
+
+		//add element to a dictionary
+		public void Add(TKey key, TValue value)
+		{
+			if (value == null)
+				throw new ArgumentNullException();
+			shouldResize();
+			int position = HashFunction(key);
+
+			if (buckets[position] == null)
+				buckets[position] = new List<KeyValuePair<TKey, TValue>>();
+
+			foreach (var pair in buckets[position])
+			{
+				if (pair.Key.Equals(key))
+					throw new ArgumentException("Key already exists");
+			}
+
+			buckets[position].Add(new KeyValuePair<TKey, TValue>(key, value));
+			numberOfElements++;
+		}
+
+		//hash function
+		private int HashFunction(TKey key)
+		{
+			if (key == null)
+				throw new ArgumentNullException();
+			return Math.Abs(key.GetHashCode()) % buckets.Length;
+		}
+
+		//does dictionary contain key 
+		public bool ContainsKey(TKey key)
+		{
+			var position = HashFunction(key);
+
+			if (this.buckets[position] == null)
+				return false;
+
+			foreach (var pair in this.buckets[position])
+			{
+				if (pair.Key.Equals(key))
+					return true;
+			}
+			return false;
+		}
+
+		// does dicitonary contain value
+		public bool ContainsValue(TValue value)
+		{
+			foreach (var bucket in this.buckets)
+			{
+				if (bucket!=null && bucket.Count > 0)
+				{
+					foreach (var pair in bucket)
+					{
+						if (pair.Value != null && pair.Value.Equals(value))
+						{
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		// remove pair that has provided key
+		public bool Remove(TKey key)
+		{
+			var position = HashFunction(key);
+			var bucket = buckets[position];
+
+			if (bucket != null)
+			{
+				for (int i = 0; i < bucket.Count; i++)
+				{
+					if (bucket[i].Key.Equals(key))
+					{
+						bucket.RemoveAt(i);
+						numberOfElements--;
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		// clear everything from dictionary
+		public void Clear()
+		{
+			foreach (var bucket in this.buckets)
+			{
+				if (bucket != null)
+					bucket.Clear();
+			}
+			numberOfElements = 0;
+		}
+
+		// get all keys
+		public IEnumerable<TKey> Keys
+		{
+			get
+			{
+				foreach (var bucket in buckets)
+				{
+					if (bucket != null)
+					{
+						foreach (var pair in bucket)
+						{
+							yield return pair.Key;
+						}
+					}
+				}
+			}
+		}
+
+		//get all values
+		public IEnumerable<TValue> Values
+		{
+			get
+			{
+				foreach (var bucket in buckets)
+				{
+					if (bucket != null)
+					{
+						foreach (var pair in bucket)
+						{
+							yield return pair.Value;
+						}
+					}
+				}
+			}
+		}
+
+}   
+
+
